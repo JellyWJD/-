abe4115a635f9ded09ef2de42679091615b10ef6
wjd <853152615@qq.com>
2018-05-04 15:21:19 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(".Cover-part1").mouseenter(function () {
+  $(".Cover-part1").animate({
+    opacity:0.8
+  })
+})
+$(".Cover").mouseleave(function () {
+  $(".Cover").animate({
+    opacity:0
+  })
+})
